fc0731f00d2299e225805051438bc47e4bc5ecb1
Joel-Irineu <irineu.joel01@gmail.com>
2022-08-13 11:32:56 -0300

    comandos de saida

--- 02-iniciando-do-zero-com-php/02-03-comandos-de-saida/index.php
+++ 02-iniciando-do-zero-com-php/02-03-comandos-de-saida/index.php
@@ -7,11 +7,36 @@ fullStackPHPClassName("02.03 - Comandos de saída");
  */
 fullStackPHPClassSession("echo", __LINE__);
 
+echo "<p>Olá, mundo!", " ", "<span class='tag'>#BoraProgramar!</span>", "</p>";
+
+$hello = "Olá, mundo!";
+$code = "<span class='tag'>#BoraProgramar!</span>";
+
+echo "<p>$hello</p>";
+echo '<p>$hello</p>';
+
+$day = "dia";
+
+echo "<p>Falta 1 $day para o evento!</p>";
+echo "<p>Faltam 2 {$day}s para o evento!</p>";
+
+echo "<h3>{$hello}</h3>";
+echo "<h4>{$hello} {$code}</h4>";
+
+echo '<h3>'.$hello.' '.$code.'</h3>';
+
+?>
+<h4><?= $hello ?></h4>
+<?php
 
 /**
  * [ print ] https://php.net/manual/pt_BR/function.print.php
  */
 fullStackPHPClassSession("print", __LINE__);
+
+print $hello;
+print $code;
+print "<h3>{$hello} {$code}</h3>";
 
 
 /**
@@ -19,20 +44,44 @@ fullStackPHPClassSession("print", __LINE__);
  */
 fullStackPHPClassSession("print_r", __LINE__);
 
+$array = [
+    "company" => "upInside",
+    "course" => 'php',
+    "class" => "comandos de saida"
+];
+
+print_r($array);
+echo "<pre>", print_r($array, return: true),"</pre>";
 
 /**
  * [ printf ] https://php.net/manual/pt_BR/function.printf.php
  */
 fullStackPHPClassSession("printf", __LINE__);
 
+$article = "<article><h1>%s</h1><p>%s</p></article>";
+$title = "{$hello} {$code}";
+$text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s";
+
+printf($article, $title, $text);
+
+echo sprintf($article, $title, $text);
 
 /**
  * [ vprintf ] https://php.net/manual/pt_BR/function.vprintf.php
  */
 fullStackPHPClassSession("vprintf", __LINE__);
 
+$company = "<article><h1>Escola %s</h1> <p>curso <b>%s</b>, aula <b>%s</b><p/p></article>";
 
+vprintf($company, $array);
+echo vprintf($company, $array);
 /**
  * [ var_dump ] https://php.net/manual/pt_BR/function.var-dump.php
  */
 fullStackPHPClassSession("var_dump", __LINE__);
+
+var_dump(
+    $array,
+    $hello,
+    $company
+);
