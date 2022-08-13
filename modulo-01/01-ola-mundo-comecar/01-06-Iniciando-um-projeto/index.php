85d3551629832fbd48366456c77a5f98ffd94246
joel-irineu <irineu.joel01@gmail.com>
2022-08-12 23:09:01 -0300

    Olá, mundo!

---
+++ modulo-01/01-ola-mundo-comecar/01-06-Iniciando-um-projeto/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FSPHP - Iniciando um projeto</title>
+    <link rel="stylesheet" href="assets/style.css">
+</head>
+<body>
+    <?php
+    /**
+     * Bloco Php com variavel start
+     * e comando de saida
+     */
+    $start = "Vamos Começar";
+    print "<h1>Olá, mundo {$start}</h1>";
+    print "<div id='js'>loading...</h1>"
+    ?>
+    <script src="assets/index.js"></script>
+</body>
+</html>
