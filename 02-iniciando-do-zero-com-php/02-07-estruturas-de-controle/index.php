c29be441a314c8cabf487fc2809dc368f2d3f0aa
joel-irineu <irineu.joel01@gmail.com>
2022-08-14 14:31:23 -0300

    Estruturas de controle

--- 02-iniciando-do-zero-com-php/02-07-estruturas-de-controle/index.php
+++ 02-iniciando-do-zero-com-php/02-07-estruturas-de-controle/index.php
@@ -9,6 +9,35 @@ fullStackPHPClassName("02.07 - Estruturas de controle");
  */
 fullStackPHPClassSession("if, elseif, else", __LINE__);
 
+$test = true;
+
+if($test){
+    var_dump(true);
+}else{
+    var_dump(false);
+}
+
+$age = 34;
+
+if($age < 20){
+    var_dump("Bandas coloridas");
+}elseif($age > 20 && $age < 50){
+    var_dump("ótimas bandas");
+}else{
+    var_dump("rock and roll raiz!");
+}
+
+$hour = 3;
+
+if($hour >= 5 && $hour < 23){
+    if($hour < 7){
+        var_dump("Bob Marley");
+    }else{
+        var_dump("After Bridge");
+    }
+}else{
+    var_dump("ZzzzZZZZZzzz");
+}
 
 /*
  * [ isset ] https://php.net/manual/pt_BR/function.isset.php
@@ -16,12 +45,45 @@ fullStackPHPClassSession("if, elseif, else", __LINE__);
  */
 fullStackPHPClassSession("isset, empty, !", __LINE__);
 
+$rock = "";
+
+if(isset($rock)){
+    var_dump("Rock And Roll");
+}else{
+    var_dump("Die");
+}
+
+$rockAndRoll = "AC/DC";
+
+if(!empty($rockAndRoll)){
+    var_dump("Rock existe e toca");
+}else{
+    var_dump("Não existe ou não está tocando");
+}
 
 /*
  * [ switch ] https://secure.php.net/manual/pt_BR/control-structures.switch.php
  */
 fullStackPHPClassSession("switch", __LINE__);
 
+$testCase = 'completed';
 
-
-
+switch($testCase){
+    case "billet_printed":
+        var_dump("boleto impresso");
+        break;
+    case "canceled":
+        var_dump("pagamento cancelado");
+        break;
+    case "past_due":
+    case "pending":
+        var_dump("aguardando pagamento");
+        break;
+    case "aproved":
+    case "completed":
+        var_dump("pagamento aprovado!");
+        break;
+    default:
+        var_dump("erro ao processar pagamento");
+        break;
+};
