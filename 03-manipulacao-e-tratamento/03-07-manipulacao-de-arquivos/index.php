25b0b9f8524901010f29fe9f407749a33f21661a
Joel-Irineu <irineu.joel01@gmail.com>
2022-08-31 12:49:18 -0300

    Manipulação de arquivos

--- 03-manipulacao-e-tratamento/03-07-manipulacao-de-arquivos/index.php
+++ 03-manipulacao-e-tratamento/03-07-manipulacao-de-arquivos/index.php
@@ -7,14 +7,60 @@ fullStackPHPClassName("03.07 - Manipulação de arquivos");
  */
 fullStackPHPClassSession("verificação", __LINE__);
 
+$file = __DIR__ . "/file.txt";
+
+if(file_exists($file) && is_file($file)){
+    echo "<p>Existe!</p>";
+}else{
+    echo "<p>NÃO Existe!</p>";
+}
 
 /*
  * [ leitura e gravação ] fopen | fwrite | fclose | file
  */
 fullStackPHPClassSession("leitura e gravação", __LINE__);
 
+if(!file_exists($file) || !is_file($file)){
+    $fileOpen = fopen($file, "w");
+    fwrite($fileOpen, "linha 01".PHP_EOL);
+    fwrite($fileOpen, "linha 02".PHP_EOL);
+    fwrite($fileOpen, "linha 03".PHP_EOL);
+    fwrite($fileOpen, "Lorem Ipsum is simply dummy text of the printing and typesetting industry.".PHP_EOL);
+    fclose($fileOpen);
+}else{
+    var_dump(
+        file($file),
+        pathinfo($file)
+    );
+}
+
+echo "<p>".file($file)[3]."</p>";
+
+$open = fopen($file, "r");
+while(!feof($open)){
+    echo "<p>".fgets($open)."</p>";
+}
+
+fclose($open);
 
 /*
  * [ get, put content ] file_get_contents | file_put_contents
  */
 fullStackPHPClassSession("get, put content", __LINE__);
+
+$getContents = __DIR__ . "/teste2.txt";
+
+if(file_exists($getContents) && is_file($getContents)){
+    echo file_get_contents($getContents);
+}else{
+    $data = "<article><h1>Robson</h1><p>CEO UpInside <br> cursos@upinside.com.br</p></article>";
+    file_put_contents($getContents, $data);
+    echo file_get_contents($getContents);
+}
+
+// unlink($getContents);
+// unlink($file);
+
+if(file_exists(__DIR__."/test3.html") && is_file(__DIR__."/test3.html")){
+    unlink(__DIR__."/test3.html");
+}
