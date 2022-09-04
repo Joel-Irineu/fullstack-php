740ffadbd8f802007cf2ad707647806228af7c09
joel-irineu <irineu.joel01@gmail.com>
2022-09-03 22:15:04 -0300

    Classes, propriedades e objetos

--- 04-php-orientado-a-objetos/04-02-classes-propriedades-e-objetos/index.php
+++ 04-php-orientado-a-objetos/04-02-classes-propriedades-e-objetos/index.php
@@ -7,14 +7,38 @@ fullStackPHPClassName("04.02 - Classes, propriedades e objetos");
  */
 fullStackPHPClassSession("classe e objeto", __LINE__);
 
+require __DIR__."/classes/User.php";
+
+$user = new User();
+
+var_dump($user);
 
 /*
  * [ propriedades ] http://php.net/manual/pt_BR/language.oop5.properties.php
  */
 fullStackPHPClassSession("propriedades", __LINE__);
 
+$user->firstName = 'Joel';
+$user->lastName = 'Iirneu';
+$user->email = 'irineu.joel01@gmail.com';
+
+var_dump($user);
+
+echo "<p>o e-mail de {$user->firstName} é {$user->lastName}</p>";
 
 /*
  * [ métodos ] São as funções que definem o comportamento e a regra de negócios de uma classe
  */
 fullStackPHPClassSession("métodos", __LINE__);
+
+$user->setFirstName('Joel');
+$user->setLastName('Irineu');
+$user->setEmail('irineu.joel01');
+
+if(!$user->setEmail('irineu.joel01')){
+    echo "<p class='trigger error'>O e-mail {$user->getEmail()} não é valido</p>";
+}
+
+$user->email = 'irineu';
+
+var_dump($user);
